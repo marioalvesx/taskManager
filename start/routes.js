c9984873cd84aad88c770e922150dcec8e727a9c
Mario Alves <marioalvesneto@hotmail.com>
2019-09-03 00:04:00 -0300

    Working on the Routes.js

--- start/routes.js
+++ start/routes.js
@@ -16,4 +16,10 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+Route.on('/').render('home')
+Route.get('/test', () => 'Hello world!')
+Route.get('/test/:id', function({ params }) {
+  return `this is the id ${params.id}`;
+})
+
+Route.get('/task', 'TaskController.index')
