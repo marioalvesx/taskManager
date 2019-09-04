5ac751bbe50a668ba2783e77f0a9db5cabc591d7
Mario Alves <marioalvesneto@hotmail.com>
2019-09-04 16:36:39 -0300

    Configuring routes and view

--- app/Controllers/Http/TaskController.js
+++ app/Controllers/Http/TaskController.js
@@ -1,8 +1,16 @@
 'use strict'
 
 class TaskController {
-  index() {
-    return 'Task';
+  index({ view }) {
+    const tasks = [
+      {title: 'Task one', body: 'This is task one'},
+      {title: 'Task two', body: 'This is task two'},
+    ]
+
+    return view.render('task', {
+      title: 'Your tasks',
+      tasks: tasks
+    })
   }
 }
 
