b59afa893213361e5a8e7eee8b35abdcdc800b81
Mario Alves <marioalvesneto@hotmail.com>
2019-09-07 23:41:09 -0300

    Applying bootstrap template

--- start/routes.js
+++ start/routes.js
@@ -16,7 +16,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('home')
+Route.on('/').render('main')
 // Route.get('/test', () => 'Hello world!')
 // Route.get('/test/:id', function({ params }) {
 //   return `this is the id ${params.id}`;
